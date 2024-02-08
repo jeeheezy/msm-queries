3a34e81beb0c510d455ead20c646a1ec0b563398
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-08 21:21:46 +0000

    Added dynamic routing, controller, and action for director pages

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,15 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def show
+    id = params["id"]
+    matching_record = Director.where({ :id => id})
+    @director = matching_record[0]
+    
+    @filmography_records = Movie.where({ :director_id => @director.id })
+
+
+    render({ :template => "director_templates/details"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index"})
+  get("/directors/:id", { :controller => "directors", :action => "show"})
 end
